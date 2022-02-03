2365b061426423b8a1a09483ca0bd399d47d4d2c
Tom Perry <tomperry7@gmail.com>
2022-02-03 12:21:47 -0700

    clean up compilation script

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
 
-## resume
+### resume
 xelatex tmperry_resume.tex ;
 xelatex tmperry_resume.tex ;
 xelatex tmperry_resume.tex ;
 convert -alpha remove -alpha off -background white -layers flatten -density 500 tmperry_resume.pdf tmperry_resume.png ;
 #open tmperry_resume.pdf ;
 
-## academic history
+### academic history
 xelatex tmperry_academichistory.tex ;
 xelatex tmperry_academichistory.tex ;
 xelatex tmperry_academichistory.tex ;
-convert -alpha remove -alpha off -background white -layers flatten -density 500 tmperry_academichistory.pdf tmperry_academichistory.png ;
+convert -alpha remove -alpha off -background white -density 500 tmperry_academichistory.pdf tmperry_academichistory.png ;
 #open  tmperry_academichistory.pdf ;
