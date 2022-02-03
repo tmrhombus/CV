fc98809ed663d5ae110650515610a801db0d5ebf
Tom Perry <tomperry7@gmail.com>
2022-02-03 11:45:32 -0700

    compile the tex

---
+++ compile.sh
@@ -0,0 +1,12 @@
+
+# resume
+xelatex tmperry_resume.tex ;
+xelatex tmperry_resume.tex ;
+xelatex tmperry_resume.tex ;
+open tmperry_resume.pdf ;
+
+# academic history
+xelatex tmperry_academichistory.tex ;
+xelatex tmperry_academichistory.tex ;
+xelatex tmperry_academichistory.tex ;
+open  tmperry_academichistory.pdf ;
