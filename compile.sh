9fd0c6313d1f4491f10d09cf7f7e45ee103905e1
Tom Perry <tomperry7@gmail.com>
2022-02-03 12:06:37 -0700

    imagemagick to get png from pdf

--- compile.sh
+++ compile.sh
@@ -1,12 +1,14 @@
 
-# resume
+## resume
 xelatex tmperry_resume.tex ;
 xelatex tmperry_resume.tex ;
 xelatex tmperry_resume.tex ;
-open tmperry_resume.pdf ;
+convert -alpha remove -alpha off -background white -layers flatten -density 500 tmperry_resume.pdf tmperry_resume.png ;
+#open tmperry_resume.pdf ;
 
-# academic history
+## academic history
 xelatex tmperry_academichistory.tex ;
 xelatex tmperry_academichistory.tex ;
 xelatex tmperry_academichistory.tex ;
-open  tmperry_academichistory.pdf ;
+convert -alpha remove -alpha off -background white -layers flatten -density 500 tmperry_academichistory.pdf tmperry_academichistory.png ;
+#open  tmperry_academichistory.pdf ;
