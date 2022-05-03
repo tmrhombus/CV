54e046a1c4ee3fc69ae3ada4cf06a166aa4cd88e
Tom Perry <tomperry7@gmail.com>
2022-05-02 20:33:39 -0600

    rm cruft

--- adaptavist/compile.sh
+++ adaptavist/compile.sh
@@ -3,14 +3,5 @@
  xelatex tmperry_resume.tex ;
  xelatex tmperry_resume.tex ;
  xelatex tmperry_resume.tex ;
-# convert -alpha remove -alpha off -background white -layers flatten -density 500 tmperry_resume.pdf tmperry_resume.png ;
  open tmperry_resume.pdf ;
 
-#### academic history
-#xelatex tmperry_supplemental.tex ;
-#xelatex tmperry_supplemental.tex ;
-#xelatex tmperry_supplemental.tex ;
-##xelatex tmperry_academichistory.tex ;
-##xelatex tmperry_academichistory.tex ;
-##convert -alpha remove -alpha off -background white -density 500 tmperry_academichistory.pdf tmperry_academichistory.png ;
-#open  tmperry_supplemental.pdf ;
